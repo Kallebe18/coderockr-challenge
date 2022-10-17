fe6dd12eaf600021498464737336485c6cb2c9cc
kallebe18 <kallebegomes18@gmail.com>
2022-10-17 16:12:12 -0300

    Global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import GlobalStyles from "./globalStyles";
 import { AppRoutes } from "./routes";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <GlobalStyles />
+      <AppRoutes />
+    </>
+  );
 }
 
 export default App;
