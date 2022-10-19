05711e84c365e9e26ab1b3c9a77f9da1ff8b7427
kallebe18 <kallebegomes18@gmail.com>
2022-10-19 16:15:30 -0300

    Defining fixed header height to layout

--- src/Layouts/Default/index.tsx
+++ src/Layouts/Default/index.tsx
@@ -1,14 +1,21 @@
+import styled from "styled-components";
 import { Header } from "../../components/Header";
+import { headerHeight } from "../../components/Header/styles";
 
 type DefaultLayoutProps = {
   children: JSX.Element | any[];
 };
 
+const ContentContainer = styled.div`
+  padding-top: ${headerHeight}px;
+  position: relative;
+`;
+
 export function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
     <>
       <Header />
-      {children}
+      <ContentContainer>{children}</ContentContainer>
     </>
   );
 }
